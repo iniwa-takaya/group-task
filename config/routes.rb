b3e0edc27fb75f1b96f72dffd23c458893ecc810
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-11 02:30:19 +0900

    タスク画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "task_groups#index"
   resources :users, only: %i[show edit update]
   resources :task_groups, only: %i[new create edit update destroy] do
-    resources :spaces, only: %i[index create edit update destroy]
+    resources :spaces, only: %i[index create edit update destroy] do
+      resources :tasks, only: %i[index]
+    end
   end
 end
