b8809e7174b0c5ece2ec9a5661685737a349ae5d
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-10 23:08:20 +0900

    スペース編集機能の実装

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,6 +1,7 @@
 class SpacesController < ApplicationController
-  before_action :find_task_group, only: %i[index create] 
-
+  before_action :find_task_group, only: %i[index create edit update] 
+  before_action :find_space, only: %i[edit update] 
+  
   def index
     @space = Space.new
     @spaces = @task_group.spaces.includes(:task_group)
@@ -16,10 +17,28 @@ class SpacesController < ApplicationController
     end
   end
 
+  def edit
+    @spaces = @task_group.spaces.includes(:task_group)
+  end
+
+  def update
+    binding.pry
+    if @space.update(space_params)
+      redirect_to task_group_spaces_path(@task_group)
+    else
+      @spaces = @task_group.spaces.includes(:task_group)
+      render :edit
+    end
+  end
+
   private
 
   def find_task_group
     @task_group = TaskGroup.find(params[:task_group_id])
+  end
+  
+  def find_space
+    @space = Space.find(params[:id])
   end
 
   def space_params
