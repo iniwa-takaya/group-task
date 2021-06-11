a7fc8fd7a2ae9672edf9477a38641ff2a26cb80a
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-12 01:35:13 +0900

    タスク作成、編集のエラー表示の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,17 +3,19 @@ class TasksController < ApplicationController
   def index
     @task = Task.new
     @task_group = TaskGroup.find(params[:task_group_id])
-    @space = @task_group.spaces.find(params[:space_id])
+    @space = @task_group.spaces.includes(:task_group).find(params[:space_id])
     @tasks = @space.tasks.includes(task_group: :space)
   end
 
   def create
     @task_group = TaskGroup.find(params[:task_group_id])
-    @space = @task_group.spaces.find(params[:space_id])
+    @space = @task_group.spaces.includes(:task_group).find(params[:space_id])
     @task = @space.tasks.new(task_params)
     if @task.save
       redirect_to task_group_space_tasks_path(@task_group, @space)
     else
+      @task_group = TaskGroup.find(params[:task_group_id])
+      @space = @task_group.spaces.includes(:task_group).find(params[:space_id])
       @tasks = @space.tasks.includes(task_group: :space)
       render :index
     end
@@ -21,7 +23,7 @@ class TasksController < ApplicationController
 
   def edit
     @task_group = TaskGroup.find(params[:task_group_id])
-    @space = @task_group.spaces.find(params[:space_id])
+    @space = @task_group.spaces.includes(:task_group).find(params[:space_id])
     @task = Task.find(params[:id])
   end
 
@@ -32,6 +34,8 @@ class TasksController < ApplicationController
     if @task.update(task_params)
       redirect_to task_group_space_tasks_path(@task_group, @space)
     else
+      @task_group = TaskGroup.find(params[:task_group_id])
+      @space = @task_group.spaces.includes(:task_group).find(params[:space_id])
       @tasks = @space.tasks.includes(task_group: :space)
       render :edit
     end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :task_group
   belongs_to :space
+
+  validates :content, presence: true 
+  validates :status,  inclusion: { in: [true, false] }
 end
