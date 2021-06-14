1c7666229e01df5f9c7d825aff9298c8018f87dc
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-14 18:32:59 +0900

    space単体テストコードの実装

--- spec/factories/spaces.rb
+++ spec/factories/spaces.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :space do
-    
+    s_name { 'abc' }
+    s_description { 'abc' }
+    association :task_group
   end
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Space, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @space = FactoryBot.build(:space)
+  end
+
+  describe 'スペース機能' do
+    
+    context 'スペースが作成できる場合'do
+      it '全ての情報があれば作成できる' do
+        expect(@space).to be_valid
+      end
+      it 's_descriptionが空でも作成できる' do
+        @space.s_description = ""
+        expect(@space).to be_valid
+      end
+    end
+
+    context 'スペースが作成できない場合' do
+      it 'スペース名が空では作成できない' do
+        @space.s_name = ""
+        @space.valid?
+        expect(@space.errors.full_messages).to include("作業スペースの名前を入力してください")
+      end
+    end
+
+  end
 end
