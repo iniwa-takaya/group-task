effd0038594ba593d6e7bd1bc9a282158974c962
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-14 18:18:21 +0900

    task_group単体テストモデルの実装

--- spec/factories/task_groups.rb
+++ spec/factories/task_groups.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task_group do
-    
+    g_name { 'abc'}
+    g_description { 'abc'}
   end
 end
--- spec/models/task_group_spec.rb
+++ spec/models/task_group_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe TaskGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @task_group = FactoryBot.build(:task_group)
+  end
+
+  describe 'グループ機能' do
+    
+    context 'グループが作成できる場合' do
+      it 'すべての情報がある場合作成できる' do
+        expect(@task_group).to be_valid
+      end
+      it 'g_descriptionが空でも登録できる' do
+        @task_group.g_description = ""
+        expect(@task_group).to be_valid
+      end
+    end
+
+    context 'グループが作成できない場合' do
+      it 'g_nameが空では作成できない' do
+        @task_group.g_name = ""
+        @task_group.valid?
+        expect(@task_group.errors.full_messages).to include("チーム名を入力してください")
+      end
+    end
+
+  end
 end
