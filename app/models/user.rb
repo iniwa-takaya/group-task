71a6bbc9afa8ec204d4dda1e6b8b1f639658d393
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-15 20:45:58 +0900

    ユーザー編集機能の修正（＃１）
    on: :createによるバリデーションの制限

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nick_name, uniqueness: { case_sensitive: false }
   end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, on: :create
 
   has_many :task_group_users
   has_many :task_groups, through: :task_group_users
