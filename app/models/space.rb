ef069ed2d9164e3fc54c44c9e711534424f33196
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-15 16:48:38 +0900

    グループ作成機能の修正（＃1）
    validate: false の追記

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,6 +1,6 @@
 class Space < ApplicationRecord
   belongs_to :task_group
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :s_name, presence: true
 end
--- app/models/task_group.rb
+++ app/models/task_group.rb
@@ -1,8 +1,8 @@
 class TaskGroup < ApplicationRecord
   has_many :task_group_users, dependent: :destroy
-  has_many :users, through: :task_group_users
+  has_many :users, through: :task_group_users, validate: false
   has_many :spaces, dependent: :destroy
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   validates :g_name, presence: true
 end
