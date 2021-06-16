1dccba04614faa5a062180c1779688e910d38d30
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-16 15:43:31 +0900

    新規登録方法選択画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "task_groups#index"
-  resources :users, only: %i[show edit update]
+  resources :users, only: %i[new show edit update]
   resources :task_groups, only: %i[new create edit update destroy] do
     resources :spaces, only: %i[index create edit update destroy] do
       resources :tasks, only: %i[index create edit update destroy]
